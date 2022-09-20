f8b2171bf56ce743581671c32a648801e12caa58
Fitz Hawke <will@fitzhawk.com>
2022-09-20 00:23:44 -0400

    add initial frontend UI framework with basic login and register

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+
+function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaSignInAlt /> Sign in
+                </h1>
+                <p>Log in and start settign goals</p>
+            </section>
+
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input
+                            type='email'
+                            class='form-control'
+                            id='email'
+                            name='email'
+                            value={email}
+                            placeholder='Enter your email'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            class='form-control'
+                            id='password'
+                            name='password'
+                            value={password}
+                            placeholder='Enter password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
+}
+
+export default Login;

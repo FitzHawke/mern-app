fb498c6bd45a7b84c77e497d24527ad39d0a510a
Fitz Hawke <will@fitzhawk.com>
2022-09-22 00:03:21 -0400

    add in backend logic for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -16,5 +17,17 @@ app.use(errorHandler);
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(port, () => console.log(`server started on port ${port}`));
